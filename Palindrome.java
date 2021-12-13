72d2b258c49c705731d32a61080cbafa99a39c69
AdityaAnn <78384160+AdityaAnn@users.noreply.github.com>
2021-12-12 20:44:36 -0500

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -2,16 +2,15 @@ package palindrome;
 import java.util.Scanner;
 
 public class Palindrome {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in); //this will declare a new scanner to read input
 		System.out.print("Enter a string with no punctuation or spaces: ");  //print out to console then read in user input 
-    //In Java all functions and variables are declared within a class thus there is no scope resolution operator. Java also does not allow for variables. in java varables must exist within a class. Unlike C++ where glooble varables exist. 
+  		//In Java all functions and variables are declared within a class thus there is no scope resolution operator. Java also does not allow for variables. in java varables must exist within a class. Unlike C++ where glooble varables exist. 
 		String palindrome = sc.next();
 		int palindromeLen = palindrome.length(); //This will find string length length
 		boolean isPal = true;
-		//This will go throught the string and will compare the first and last characters. Then It will incrementing and decrementing the iterators respectively. 
-		//If there is a proint when the program does not find a difference and they are not equal, the program stops running.
+		//This will go throught the string and will compare the first and last characters. Then It will incrementing and decrementing the iterators. 
+		//If there is a proint when the program does find a difference, the program stops running.
 		for (int i=0; i < palindrome.length(); i++) {
 			if (palindrome.charAt(i) != palindrome.charAt(palindromeLen-1)) {
 				System.out.print(palindrome);  //Java instead of having overloaded IO operators it has special print functions and scanners, and instead of indexing it has charAt().
@@ -27,13 +26,10 @@ public class Palindrome {
 		}
 		//close the scanner to prevent resource loss
 		sc.close();
-	}
-			
+	}		
 }
 
 //Compilation
-//Java compiles and runs very differently from most other languages first compiling on a platform
-//independent compiler then running on a platform dependent virtual machine called the JVM, this is
-//why java is able to be compiled once and run anywhere. First the compiler takes the .java source
-//code and translates it into bytecode. Then the bytecode is passed to the JVM where is is interpreted
-//for the machine to run.
+//Java uses an independent compiler then running on a platform dependent virtual machine called the JVM.
+//That is why java is able to be compiled once and run anywhere.
+//First the compiler takes the .java source code and translates it into bytecode. Then the bytecode is passed to the JVM where is is interpreted for the machine to run.
